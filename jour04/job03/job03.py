9c2cb3967d84c4e7452f76e6b8cf7ade868c8a3a
Crystal-Destailleurs <146716248+Crystal-Destailleurs@users.noreply.github.com>
2023-11-23 12:47:43 +0100

    job03

---
+++ jour04/job03/job03.py
@@ -0,0 +1,8 @@
+def ajouter_melon():
+    fruits = ["pomme", "cerise", "orange"]
+   
+    fruits.append("Melon")
+  
+    print("La liste de fruits après ajout de 'Melon' :", fruits)
+
+ajouter_melon()
