034872bdafbb406d009736e5f89fec7f759c3311
Crystal-Destailleurs <146716248+Crystal-Destailleurs@users.noreply.github.com>
2023-11-23 13:38:31 +0100

    job09

---
+++ jour04/job09/job09.py
@@ -0,0 +1,14 @@
+# Liste fournie
+L = [8, 24, 27, 48, 2, 16, 9, 102, 7, 84, 91]
+
+# Récupérer la valeur maximale, la valeur minimale et la somme
+valeur_maximale = max(L)
+valeur_minimale = min(L)
+somme_elements = sum(L)
+
+# Afficher les résultats
+print("Liste :", L)
+print("Valeur maximale :", valeur_maximale)
+print("Valeur minimale :", valeur_minimale)
+print("Somme des éléments :", somme_elements)
+    
