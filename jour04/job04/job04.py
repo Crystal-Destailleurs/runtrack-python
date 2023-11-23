83e0a47a81f902346865036b405e89c53a3307a1
Crystal-Destailleurs <146716248+Crystal-Destailleurs@users.noreply.github.com>
2023-11-23 12:51:59 +0100

    job04

---
+++ jour04/job04/job04.py
@@ -0,0 +1,12 @@
+def ajouter_mangue():
+    fruits = ["pomme", "cerise", "orange", "Melon"]
+    
+
+    fruits.insert(2, "Mangue")
+    
+
+    print("La liste de fruits après ajout de 'Mangue' à l'index 2 :", fruits)
+
+
+ajouter_mangue()
+
