f4d68090fb4de6f12f999c1169c68e37958630de
Crystal-Destailleurs <146716248+Crystal-Destailleurs@users.noreply.github.com>
2023-11-24 15:44:32 +0100

    correction

--- jour05/job03/job03.py
+++ jour05/job03/job03.py
@@ -1,15 +1,12 @@
 def draw_triangle(height):
     for i in range(height):
         spaces = ' ' * (height - i - 1)
-        if i == 0:
-        
-            print(spaces + '_')
-        elif i == height - 1:
+        if i == height - 1:
   
-            print('/' + '_' * (2 * i - 1) + '\\')
+            print('/' + '_' * (2 * i ) + '\\')
         else:
 
-            print(spaces + '/' + ' ' * (2 * i - 1) + '\\')
+            print(spaces + '/' + ' ' * (2 * i ) + '\\')
 
 hauteur = int(input("Entrez la hauteur du triangle : "))
 
